604a015410207c87886a5c4c15f81de0d835347b
annaKulyk <ankakulik1998@gmail.com>
2021-08-17 12:11:27 +0300

    js live coding

---
+++ javascript/functions/task1/index.js
@@ -0,0 +1,66 @@
+// console.log('Hello!');
+
+// function getSenseOfLife() {
+//   console.log(42);
+// }
+
+// const res = getSenseOfLife();
+// console.log(res);
+
+// console.log(getSenseOfLife());
+
+// function getSquared(num) {
+//   return num * num;
+// }
+
+// console.log(getSquared(5));
+// console.log(getSquared(0));
+// console.log(getSquared(-4));
+
+// function sum(firstNum, secondNum) {
+//   return firstNum + secondNum;
+// }
+
+// console.log(sum(1, 2));
+// console.log(sum(-1, 6));
+// console.log(sum(0, 2));
+// console.log(sum(1, 2, 100));
+
+// function fetMessage(age) {
+//   console.log('I am ' + age + ' years old');
+// }
+
+// console.log(fetMessage(15));
+// console.log(fetMessage(100));
+
+//option 1
+
+// let mult = (firstNumber, secondNumber) => {
+//   return firstNumber * secondNumber;
+// };
+
+//option 2
+
+//let mult = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+//option 3
+
+// let mult = function (firstNumber, secondNumber = 1) {
+//   return firstNumber * secondNumber;
+// };
+
+// console.log(mult(1));
+// console.log(mult(3, 5));
+// console.log(mult);
+
+// const square = number => number * number;
+
+// console.log(square(2));
+// console.log(square(-50));
+// console.log(square(0));
+
+// const getMagicNumber = () => 17;
+
+// console.log(getMagicNumber());
+// console.log(getMagicNumber);
+// console.log(getMagicNumber(2));
